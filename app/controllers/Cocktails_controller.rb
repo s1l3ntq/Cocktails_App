e7ab7d5705c960a082099d2d51a5de38bf8d68c7
s1l3ntq <qimiller89@gmail.com>
2021-01-10 13:33:09 -0600

    added new erv and updated index erb

--- app/controllers/Cocktails_controller.rb
+++ app/controllers/Cocktails_controller.rb
@@ -1,11 +1,29 @@
 
 
 class CocktailsController < ApplicationController
+    get '/cocktails/new' do
+        erb :'/cocktails/new'
+    end
 
+    post '/cocktails' do
+        cocktail = Cocktail.new(params)
+        if !cocktail.name.empty? && !cocktail.ingredient1.empty?
+            cocktail.save
+            redirect '/cocktails'
+        else
+            @error = "User must input Title and Ingredients to continue."
+            erb :'/cocktails/new'
+        end
+    end
 
-    get '/cocktails' do
+    get '/cocktails' do #Read
         @cocktails = Cocktail.all
         erb :'/cocktails/index'
+    end
+
+    get '/cocktails/:id' do #Read
+        @cocktail = Cocktail.find(params['id'])
+        erb :'/cocktails/show'
     end
     
 end
